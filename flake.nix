43b60bab9f7fe307d4a3507a72364cce620ac850
00y300 <navajerry83@gmail.com>
2025-09-21 18:34:22 -0400

    Inital Commit
    Intial commit let's get going

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Node.js development environment with latest Node.js and local bin path";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        nodejs = pkgs.nodejs_24;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            eslint_d
+            vscode-js-debug
+            vscode-extensions.firefox-devtools.vscode-firefox-debug
+            nodePackages.typescript
+            # nodePackages.pnpm
+            # nodePackages.yarn
+            nodePackages.prettier
+            typescript-language-server # Using pkgs directly for typescript-language-server
+            typescript
+          ];
+
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+          '';
+        };
+      }
+    );
+}
